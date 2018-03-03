f852d32c50ca817c4744d0e81422c5628101a16a
Franco Peña <fpenac@pucp.pe>
2018-03-03 22:49:50 +0100

    Remove ignored files

--- inginious-problems-permutation/static/task_permutation.js
+++ inginious-problems-permutation/static/task_permutation.js
@@ -1,5 +1,7 @@
 function load_input_permutation(submissionid, key, input)
 {
+    var grid = new Muuri('.grid');
+    
     var field = $("form#task input[name='" + key + "']");
     if(key in input)
         $(field).prop('value', input[key]);
