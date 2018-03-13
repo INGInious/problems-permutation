a1d405444150d9bc116699d7ba49ed2f2209d1e2
Franco Peña <fpenac@pucp.pe>
2018-03-13 12:14:11 +0100

    Added permutation list support

---
+++ permutation-task/src/PermutationList.jsx
@@ -0,0 +1,93 @@
+/* @flow */
+import Item from './Item';
+import Muuri from 'muuri';
+import { IdManager } from './IdManager';
+
+
+export default class PermutationList {
+    columnGrid: Muuri;
+    listContainer: HTMLElement;
+
+    answers: Set<Item>;
+    itemsMap: {[string]: Item};
+
+    constructor(listContainer: HTMLElement, items: Array<Item>) {
+        var that = this;
+
+        this.listContainer = listContainer;
+
+        this.answers = new Set();
+		this.itemsMap = {};
+		for(let i=0;i<items.length;i++) {
+			this.itemsMap[items[i].get_id()] = items[i];
+			this.answers.add(items[i]);
+        }
+        
+        this.generate_muuri(listContainer, function(item:Item) {
+            var sorted: Array<Item> = that.get_sorted_items(that.answers);
+            for(let i=0;i<sorted.length;i++) {
+                sorted[i].update_position(i+1);// Starts in 1
+            }
+		})
+    }
+
+    get_sorted_items(unsorted: Set<Item>) {
+		var sorted: Array<Item> = [];
+
+		var posItemPair: Array<[number, Item]> = [];
+		unsorted.forEach(function(item) {
+			posItemPair.push([item.get_y(), item]);
+		});
+		
+		posItemPair = posItemPair.sort(function(a:[number, Item], b:[number, Item]): number {
+			return a[0] > b[0]? 1: -1;
+		});
+		
+		for(let i=0;i<posItemPair.length;i++) sorted.push(posItemPair[i][1]);
+
+		return sorted;
+	}
+
+    generate_muuri(container: HTMLElement, process_item: (Item)=>void) {
+        var that = this;
+
+		var grid = new Muuri(container, {
+			items: '.permutation-item',
+			layoutDuration: 400,
+			layoutEasing: 'ease',
+			dragEnabled: true,
+			dragSort: function () {
+				return [that.columnGrid];
+			},
+			dragSortInterval: 0,
+			dragContainer: document.body, // TODO: can we drag items in the whole body?
+			dragReleaseDuration: 400,
+			dragReleaseEasing: 'ease'
+		})
+		.on('dragStart', function (item) {
+			// Let's set fixed widht/height to the dragged item
+			// so that it does not stretch unwillingly when
+			// it's appended to the document body for the
+			// duration of the drag.
+			item.getElement().style.width = item.getWidth() + 'px';
+			item.getElement().style.height = item.getHeight() + 'px';
+			//console.log('Pressed element ' + item._child.getAttribute('name') + ' at ' + name);
+		})
+		.on('dragReleaseEnd', function (item) {
+			// Let's remove the fixed width/height from the
+			// dragged item now that it is back in a grid
+			// column and can freely adjust to it's
+			// surroundings.
+			item.getElement().style.width = '';
+			item.getElement().style.height = '';
+			// Just in case, let's refresh the dimensions of all items
+			// in case dragging the item caused some other items to
+			// be different size.
+			that.columnGrid.refreshItems();
+			if(process_item!=null) process_item(that.itemsMap[item._child.getAttribute('id')]);
+			//console.log('Released element ' + item._child.getAttribute('name'));
+		});
+
+		that.columnGrid = grid;
+    }
+}
--- permutation-task/src/index.jsx
+++ permutation-task/src/index.jsx
@@ -2,24 +2,10 @@
 import GridSystem from './GridSystem';
 import { IdManager } from './IdManager';
 import Item from './Item';
+import PermutationList from './PermutationList';
 
 
-const itemFactory = (elemId: string, text: string) => {
-	const item = document.createElement('div')
-	item.setAttribute('class', 'permutation-item')
-	
-	const itemContent = document.createElement('div')
-	itemContent.setAttribute('id', elemId)
-	itemContent.setAttribute('class', 'permutation-item-content')
-	itemContent.innerHTML = text;
-	item.appendChild(itemContent)
-
-	return item
-}
-
-export function generate_ui(pid: string, elems: Array<string>, elemsId: Array<string>) {
-	IdManager.init(pid);
-
+function generate_trello_ui(pid: string, elems: Array<string>, elemsId: Array<string>) {
 	var nullAnswersContainer: HTMLElement|null = document.getElementById(IdManager.stringify(IdManager.TPL_ANSWER_CONTAINER))
 	var nullCandidatesContainer: HTMLElement|null = document.getElementById(IdManager.stringify(IdManager.TPL_CANDIDATES_CONTAINER))
 	var nullHiddenInputs: HTMLElement|null = document.getElementById(IdManager.stringify(IdManager.TPL_HIDDEN_INPUTS))
@@ -45,6 +31,43 @@ export function generate_ui(pid: string, elems: Array<string>, elemsId: Array<st
 		items.push(item)
 	}
 	new GridSystem(answersContainer, candidatesContainer, items)
+}
+
+function generate_list_ui(pid: string, elems: Array<string>, elemsId: Array<string>) {
+	var nullAnswersContainer: HTMLElement|null = document.getElementById(IdManager.stringify(IdManager.TPL_ANSWER_CONTAINER))
+	var nullHiddenInputs: HTMLElement|null = document.getElementById(IdManager.stringify(IdManager.TPL_HIDDEN_INPUTS))
+
+	if(nullAnswersContainer==null || nullHiddenInputs==null) return;
+
+	const answersContainer: HTMLElement = nullAnswersContainer;
+	const hiddenInputs: HTMLElement = nullHiddenInputs;
+
+	var items: Array<Item> = [];
+
+	for(let i=0;i<elems.length;i++) {
+		var item: Item;
+
+		item = new Item(i+1, elemsId[i], elems[i]) // Starts in 1
+		item.build()
+
+		answersContainer.appendChild(item.get_card())
+		hiddenInputs.appendChild(item.get_iposHiddenInput())
+		hiddenInputs.appendChild(item.get_posHiddenInput())
+
+		items.push(item)
+	}
+	new PermutationList(answersContainer, items)
+}
+
+export function generate_ui(ptype: 'trello'|'list', pid: string, elems: Array<string>, elemsId: Array<string>) {
+	IdManager.init(pid);
+
+	if(ptype=='trello') {
+		generate_trello_ui(pid, elems, elemsId);
+	}
+	if(ptype=='list') {
+		generate_list_ui(pid, elems, elemsId);
+	}
 }
 
 
