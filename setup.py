1b7f4eb3d13b98eb3f422eb731722820ea8ab5bc
Franco Peña <fpenac@pucp.pe>
2018-03-23 18:30:20 +0100

    Adding argument to partially build module

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 
 from setuptools import setup, find_packages
 from os import system
+from sys import argv
 
 import inginious_blockly
 
-system("cd permutation-task && npm run-script build")
+if not 'no-build' in argv:
+    system("cd permutation-task && npm run-script build")
 
 setup(
     name="inginious-problems-permutation",
