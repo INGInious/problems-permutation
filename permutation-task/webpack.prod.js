64e0588bcab9806e1b2685b1794b70db14509258
Franco Peña <fpenac@pucp.pe>
2018-03-23 18:29:27 +0100

    Renaming bundle

--- permutation-task/webpack.prod.js
+++ permutation-task/webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = {
 		index: './src/index.jsx',
 	},
 	output: {
-		filename: '../../inginious-problems-permutation/static/task/bundle.js',
+		filename: '../../inginious-problems-permutation/static/task/permutation_task.js',
 		path: path.resolve(__dirname, 'dist'),
 		libraryTarget: 'var',
 		library: 'PermutationTaskUI',
