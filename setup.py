94b5ff14fe98ea6362d13c0bc42fd2d47bd72f72
Franco Peña <fpenac@pucp.pe>
2018-05-02 14:16:46 +0200

    Using yarn when setup

--- setup.py
+++ setup.py
@@ -4,13 +4,31 @@
 from setuptools import setup, find_packages
 from os import system
 from sys import argv
+from shutil import which
 
 import inginious_blockly
 
+yarnInstalled = which('yarn') != None
+npmInstalled = which('npm') != None
+
+if not yarnInstalled and not npmInstalled:
+    print("To build this plugin you require to have installed node.")
+    pass
+elif not yarnInstalled:
+    print("It is recommended to have installed yarn to build this project.")
+
 if not 'no-npm' in argv:
-    system("cd permutation-task && npm install")
+    
+    if yarnInstalled:
+        system("cd permutation-task && yarn install")
+    elif npmInstalled:
+        system("cd permutation-task && npm install")
+
     if not 'no-npm-build' in argv:
-        system("cd permutation-task && npm run-script build")
+        if yarnInstalled:
+            system("cd permutation-task && yarn run build")
+        elif npmInstalled:
+            system("cd permutation-task && npm run-script build")
 
 setup(
     name="inginious-problems-permutation",
