2c1de2ad11f618ee07d95a29b2ae06edeff5648a
Franco Peña <fpenac@pucp.pe>
2018-05-02 12:47:53 +0200

    Using different port than studio

--- permutation-task/webpack.dev.js
+++ permutation-task/webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
-        port: 9000
+        port: 9001
     },
 	entry: {
 		index: './src/index.jsx',
