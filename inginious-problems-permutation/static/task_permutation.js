1c93b91b66e0962177432371b840e55ea2dfb4cb
Franco Peña <fpenac@pucp.pe>
2018-07-05 21:57:19 +0200

    Removing deprecated overload

--- inginious-problems-permutation/static/task_permutation.js
+++ inginious-problems-permutation/static/task_permutation.js
@@ -1,7 +1,25 @@
 
 var uiTaskObject = {}
 
-window.onload = function () {
+// Using solution from
+// https://stackoverflow.com/questions/9568006/prevent-window-onload-being-overridden-javascript/9568836#9568836
+// window.attachEvent
+function addEvent(node, type, listener) {
+    if (node.addEventListener) {
+        node.addEventListener(type, listener, false);
+        return true;
+    } else if (node.attachEvent) {
+        node['e' + type + listener] = listener;
+        node[type + listener] = function() {
+            node['e' + type + listener](window.event);
+        }
+        node.attachEvent('on' + type, node[type + listener]);
+        return true;
+    }
+    return false;
+};
+
+addEvent(window, 'load', function () {
     // PermutationTask.generate_permutation_list_with([Array(110).join(' C-C C+C ') + ';','D','A','B'], '.board-column-content');
     //PermutationTask.generate_permutation_list('.board-column-content');
     //alert(document.querySelector('.board-column-content').getAttribute('data'));
@@ -25,7 +43,7 @@ window.onload = function () {
 
         uiTaskObject[pid] = PermutationTaskUI.generate_ui(ptype, pid, elems, elemsId, tables, containerColor);
     }
-}
+});
 
 
 function load_input_permutation(submissionid, key, input)
